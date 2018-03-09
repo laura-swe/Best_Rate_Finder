24b0a9f1cf070e46887651e9974a133b48a59215
Laura <lauraguevara97@hotmail.com>
2018-03-08 23:30:32 -0800

    Create install_requirements.sh

---
+++ install_requirements.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#install_requirements.sh
+#author: @LGuevara
+#-----------------------
+echo "Installing Requirements to run program..."
+
+pip3 install et-xmlfile
+pip3 install pandas
+pip3 install lxml
+pip3 install openpyxl
+pip3 install requests
